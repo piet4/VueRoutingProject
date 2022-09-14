87de3c3a9dae9344a3ed638bf6ae4f6619af22a7
piet4 <pe7er4@gmail.com>
2022-09-14 23:31:27 +0200

    Routing Init

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Films></Films> 
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
+import Films from '@/components/Films.vue'
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+    Films,
   }
 }
 </script>
